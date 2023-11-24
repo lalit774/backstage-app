dd2940c9b8938a1c11456857abcaf7a05fe7a54d
lalit kumar <lalit.kumar@socialinnovation.ca>
2023-11-23 23:41:14 -0500

    add todo list plugin

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -33,6 +33,7 @@ import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
 import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
 import { RequirePermission } from '@backstage/plugin-permission-react';
 import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
+import { TodoListPage } from '@internal/plugin-todo-list';
 
 const app = createApp({
   apis,
@@ -92,6 +93,7 @@ const routes = (
       {searchPage}
     </Route>
     <Route path="/settings" element={<UserSettingsPage />} />
+    <Route path="/todo-list" element={<TodoListPage />} />
     <Route path="/catalog-graph" element={<CatalogGraphPage />} />
   </FlatRoutes>
 );
---
+++ packages/backend/src/plugins/todolist.ts
@@ -0,0 +1,17 @@
+import { DefaultIdentityClient } from '@backstage/plugin-auth-node';
+import { createRouter } from '@internal/plugin-todo-list-backend';
+import { Router } from 'express';
+import { PluginEnvironment } from '../types';
+
+export default async function createPlugin({
+  logger,
+  discovery,
+}: PluginEnvironment): Promise<Router> {
+  return await createRouter({
+    logger,
+    identity: DefaultIdentityClient.create({
+      discovery,
+      issuer: await discovery.getExternalBaseUrl('auth'),
+    }),
+  });
+}
